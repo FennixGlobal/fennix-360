1e803281bc09b93fe524161a105cce73ec8d1dcd
Sindhura Gudarada <YATRAONLINE\sindhura.gudarada@lap0002706.yatraonline.local>
2018-05-04 02:29:31 +0530

    queries related to user master data

--- fennix-backend-app/web-controller/user-controller.js
+++ fennix-backend-app/web-controller/user-controller.js
@@ -5,20 +5,46 @@ var router = express.Router();
 /* GET home page. */
 router.post('/metadata', function(req, res) {
     var returnObj = {};
-    userAccessor.getMetadata(req.body).then((doc) => {
-        if (doc !== null && doc !== undefined) {
-            returnObj = {
-                status: 200,
-                message: 'User Metadata available',
-                data: doc[0].masterdata
-            }
+    userAccessor.getMetadata(req.body).then((doc, err) => {
+        // if (doc !== null && doc !== undefined) {
+        //     returnObj = {
+        //         status: 200,
+        //         message: 'User Metadata available',
+        //         data: doc[0].masterdata
+        //     }
+        // } else {
+        //     returnObj = {
+        //         status: 611,
+        //         message: 'User Metadata unavailable',
+        //         data: null
+        //     }
+        // }
+
+        if (err) {
+            console.log(err);
         } else {
-            returnObj = {
-                status: 611,
-                message: 'User Metadata unavailable',
-                data: null
-            }
+            // console.log(res.rows);
+            data = JSON.parse(JSON.stringify(doc.rows));
+            returnObj = data.reduce(function(init,item){
+                var obj = {
+                    widget_id:item.widget_id,
+                }
+                var obj2 = {
+                    card_id:item.cardid
+                }
+                if(init.hasOwnProperty(item.cardid)){
+                    debugger;
+                    init[item.cardid]['widgets'].push(obj);
+                }else {
+                    init[item.cardid] = obj2;
+                    init[item.cardid]['widgets'] = [obj];
+                }
+                return init;
+            },{});
+            console.log(data);
+            console.log(returnObj);
         }
+
         res.send(returnObj);
     })
 });
