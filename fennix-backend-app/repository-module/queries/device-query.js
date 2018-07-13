85e5d20806b329acfda219fdbad4db41fc7ae50c
Abhay lokesh <abjhontyharsh@gmail.com>
2018-07-14 03:40:06 +0530

    location map query modification

--- fennix-backend-app/repository-module/queries/device-query.js
+++ fennix-backend-app/repository-module/queries/device-query.js
@@ -1,4 +1,4 @@
-const {deviceAggregator,deviceTypeModel,DeviceCounter} = require('../models/device-model');
+const {deviceAggregator, deviceTypeModel, DeviceCounter} = require('../models/device-model');
 
 const userIdDeviceAggregatorQuery = (query) => {
     return deviceAggregator.aggregate().match({"beneficiaryId": {$in: query}})
@@ -25,7 +25,49 @@ const getDeviceDetailsForListOfBeneficiariesQuery = (query) => {
             "deviceType.name": 1
         });
 };
-
+//
+// const deviceDetailsByBeneficiaryId = (query) => {
+//     return deviceAggregator.aggregate([
+//         {
+//             $match: {
+//                 "beneficiaryId": {"$in": query}
+//             }
+//         },
+//         {
+//             $lookup: {
+//                 from: "deviceAttributes",
+//                 localField: "beneficiaryId",
+//                 foreignField: "beneficiaryId",
+//                 as: "deviceAttributes"
+//             }
+//         },
+//         {
+//             $unwind: "$deviceAttributes"
+//         },
+//         {
+//             $unwind: "$deviceAttributes.locationDetails"
+//         },
+//         {
+//             $sort: {
+//                 "deviceAttributes.locationDetails.deviceUpdatedDate": -1
+//             }
+//         },
+//         {
+//             $group: {
+//                 _id: "$_id",
+//                 latestBeneficiaryDeviceDetails: {"$first": "$$CURRENT"}
+//             }
+//         },
+//         {
+//             $lookup: {
+//                 from: "deviceTypes",
+//                 localField: "latestBeneficiaryDeviceDetails.deviceTypeId",
+//                 foreignField: "_id",
+//                 as: "deviceType"
+//             }
+//         }
+//     ]);
+// };
 const deviceDetailsByBeneficiaryId = (query) => {
     return deviceAggregator.aggregate([
         {
@@ -45,14 +87,6 @@ const deviceDetailsByBeneficiaryId = (query) => {
             $unwind: "$deviceAttributes"
         },
         {
-            $unwind: "$deviceAttributes.locationDetails"
-        },
-        {
-            $sort: {
-                "deviceAttributes.locationDetails.deviceUpdatedDate": -1
-            }
-        },
-        {
             $group: {
                 _id: "$_id",
                 latestBeneficiaryDeviceDetails: {"$first": "$$CURRENT"}
@@ -68,127 +102,127 @@ const deviceDetailsByBeneficiaryId = (query) => {
         }
     ]);
 };
-
-const listDevicesQuery = (query) => {
-    return deviceAggregator.aggregate([
-        {
-            $match: {
-                "centerId": {$in:query}
+    const listDevicesQuery = (query) => {
+        return deviceAggregator.aggregate([
+            {
+                $match: {
+                    "centerId": {$in: query}
+                }
+            },
+            {
+                $lookup: {
+                    from: "deviceTypes",
+                    localField: "deviceTypeId",
+                    foreignField: "_id",
+                    as: "deviceTypes"
+                }
+            },
+            {
+                $lookup: {
+                    from: "simcards",
+                    localField: "simCardId",
+                    foreignField: "_id",
+                    as: "simcards"
+                }
+            },
+            {
+                $unwind: "$deviceTypes"
+            },
+            {
+                $unwind: "$simcards"
+            },
+            {
+                $project: {
+                    "imei": 1,
+                    "deviceTypes.name": 1,
+                    "simcards.phoneNo": 1,
+                    "isActive": 1,
+                    "centerId": 1,
+                    "beneficiaryId": 1
+                }
             }
-        },
-        {
-            $lookup:{
-                from: "deviceTypes",
-                localField:"deviceTypeId",
-                foreignField: "_id",
-                as:"deviceTypes"
-            }
-        },
-        {
-            $lookup:{
-                from: "simcards",
-                localField:"simCardId",
-                foreignField:"_id",
-                as:"simcards"
-            }
-        },
-        {
-            $unwind:"$deviceTypes"
-        },
-        {
-            $unwind:"$simcards"
-        },
-        {
-            $project: {
-                "imei":1,
-                "deviceTypes.name":1,
-                "simcards.phoneNo":1,
-                "isActive":1,
-                "centerId": 1,
-                "beneficiaryId": 1
-            }
-        }
-    ])
-};
+        ])
+    };
 
-const listDeviceTypesQuery = () => {
-    return deviceTypeModel.find(
-        {
-            "isActive":true
-        },
-        {
-            "name":1
-        }
-    );
-};
+    const listDeviceTypesQuery = () => {
+        return deviceTypeModel.find(
+            {
+                "isActive": true
+            },
+            {
+                "name": 1
+            }
+        );
+    };
 
-const insertDeviceQuery = (req) => {
-    let deviceObj = new deviceAggregator(req);
-    deviceObj.save(function (err) {
-        if (err) return console.error(err);
-    });
-};
+    const insertDeviceQuery = (req) => {
+        let deviceObj = new deviceAggregator(req);
+        deviceObj.save(function (err) {
+            if (err) return console.error(err);
+        });
+    };
 
-const fetchNextPrimaryKeyQuery = () => {
-    return DeviceCounter.find();
-};
+    const fetchNextPrimaryKeyQuery = () => {
+        return DeviceCounter.find();
+    };
 
 //TODO: add retry logic for failure conditions
-const insertNextPrimaryKeyQuery = (req) => {
-    DeviceCounter.update({_id: req}, {$inc: {counter:1}}).then(doc => {
-        if (!doc) {
-            console.log('error');
-        } else {
-            console.log('success');
-        }
-    });
-};
+    const insertNextPrimaryKeyQuery = (req) => {
+        DeviceCounter.update({_id: req}, {$inc: {counter: 1}}).then(doc => {
+            if (!doc) {
+                console.log('error');
+            } else {
+                console.log('success');
+            }
+        });
+    };
 
-const getDeviceDetailsByDeviceIdQuery = (req) => {
-    return deviceAggregator.aggregate([
-        {
-            $match:{
-                _id:req.deviceId
+    const getDeviceDetailsByDeviceIdQuery = (req) => {
+        return deviceAggregator.aggregate([
+            {
+                $match: {
+                    _id: req.deviceId
+                }
+            },
+            {
+                $lookup: {
+                    from: 'simcards',
+                    localField: 'simCardId',
+                    foreignField: '_id',
+                    as: 'simcards'
+                }
+            },
+            {
+                $lookup: {
+                    from: 'deviceTypes',
+                    localField: 'deviceTypeId',
+                    foreignField: '_id',
+                    as: 'deviceType'
+                }
+            },
+            {$unwind: '$simcards'},
+            {$unwind: '$deviceType'},
+            {
+                $project: {
+                    'imei': 1,
+                    'isActive': 1,
+                    'simcards.phoneNo': 1,
+                    'simcards.simCardType': 1,
+                    'firmwareVersion': 1,
+                    'deviceType.name': 1
+                }
             }
-        },
-        {
-            $lookup:{
-                from:'simcards',
-                localField:'simCardId',
-                foreignField:'_id',
-                as:'simcards'
-            }
-        },
-        {
-            $lookup:{
-                from:'deviceTypes',
-                localField:'deviceTypeId',
-                foreignField:'_id',
-                as:'deviceType'
-            }
-        },
-        {$unwind:'$simcards'},
-        {$unwind:'$deviceType'},
-        {$project:{
-                'imei':1,
-                'isActive':1,
-                'simcards.phoneNo':1,
-                'simcards.simCardType':1,
-                'firmwareVersion':1,
-                'deviceType.name':1
-            }
-        }
-    ])
-};
+        ])
+    };
 
-module.exports = {
-    userIdDeviceAggregatorQuery,
-    deviceDetailsByBeneficiaryId,
-    getDeviceDetailsForListOfBeneficiariesQuery,
-    listDevicesQuery,
-    listDeviceTypesQuery,
-    insertDeviceQuery,
-    insertNextPrimaryKeyQuery,
-    fetchNextPrimaryKeyQuery,
-    getDeviceDetailsByDeviceIdQuery
-};
+    module.exports = {
+        userIdDeviceAggregatorQuery,
+        deviceDetailsByBeneficiaryId,
+        getDeviceDetailsForListOfBeneficiariesQuery,
+        listDevicesQuery,
+        listDeviceTypesQuery,
+        insertDeviceQuery,
+        insertNextPrimaryKeyQuery,
+        fetchNextPrimaryKeyQuery,
+        getDeviceDetailsByDeviceIdQuery
+    };
