9326c8f907af0260b69b3e98b698a2111e834acc
Abhay lokesh <abjhontyharsh@gmail.com>
2018-09-17 23:38:38 +0530

    userids list for show map data

--- fennix-backend-app/repository-module/data-accesors/beneficiary-accesor.js
+++ fennix-backend-app/repository-module/data-accesors/beneficiary-accesor.js
@@ -64,6 +64,7 @@ const getBeneficiaryDetailsAccessor = async (req) => {
 const getBeneifciaryIdList = async (req) => {
     let returnObj, userIds, extraQuery, modifiedQuery;
     userIds = userAccessor.getUserIdsForAllRolesAccessor(req, COMMON_CONSTANTS.FENNIX_USER_DATA_MODIFIER_USER_USERID);
+    console.log(userIds);
     extraQuery= ` and center_id = $${userIds.length + 1} order by $${userIds.length + 2} desc nulls last offset $${userIds.length + 3} limit $${userIds.length + 4}`;
     modifiedQuery = `$${beneficiaryQueries.getBenefeciaryIdListForOwnerAndCenterQuery} $${extraQuery}`;
     returnObj = await connectionCheckAndQueryExec([...userIds, req.query.centerId, req.query.sort, req.query.skip, req.query.limit], modifiedQuery);
