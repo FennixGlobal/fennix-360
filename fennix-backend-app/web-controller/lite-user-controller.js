8b52bf17bc7e49bf5be17ab130a90598f1d4588e
arupnayak <arup-ams@meraas.ae>
2018-10-14 09:18:06 +0400

    added route for liteUser

--- fennix-backend-app/web-controller/lite-user-controller.js
+++ fennix-backend-app/web-controller/lite-user-controller.js
@@ -4,8 +4,6 @@ var router = express.Router();
 
 
 router.get("/liteUsers", (req, res) => {
-    console.log("base url",req.baseUrl);
-    console.log("url",req.url);
     console.log("query", req.query);
     
     LiteUser.find(req.body, (err, data) => {
@@ -18,17 +16,7 @@ router.get("/liteUsers", (req, res) => {
     });
 });
 
-router.all("/", (req, res) => {
-    console.log("base url",req.baseUrl);
-    console.log("url",req.url);
-    console.log("req", req);
-    res.status(200).send("lite-user-controller");
-    return;
-});
-
 router.post("/liteUser", (req, res) => {
-    console.log("base url",req.baseUrl);
-    console.log("url",req.url);
     console.log("body", req.body);
     LiteUser.save( (err, data) => {
         if(err){
