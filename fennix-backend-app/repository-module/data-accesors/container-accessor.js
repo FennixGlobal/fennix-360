5f1d3d3a6709aea7909aa15bebea1a6289512b65
abhay lokesh <abjhontyharsh@gmail.com>
2018-11-01 19:59:50 +0530

    added log for testing

--- fennix-backend-app/repository-module/data-accesors/container-accessor.js
+++ fennix-backend-app/repository-module/data-accesors/container-accessor.js
@@ -24,7 +24,8 @@ const listContainersAccessor = async (req) => {
     console.log(modifiedQuery);
     finalQuery = `${modifiedQuery} ${sortWithPaginationQueryCreator(req.sortBy, 'desc', parseInt(req.skip,10), parseInt(req.limit,10))}`;
     console.log(finalQuery);
-    returnObj = await connectionCheckAndQueryExec(req.userIdList, finalQuery);
+    console.log(req.userIdList);
+    returnObj = await connectionCheckAndQueryExec([...req.userIdList], finalQuery);
     return returnObj;
 };
 
