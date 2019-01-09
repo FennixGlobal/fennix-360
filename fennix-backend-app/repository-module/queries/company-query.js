d24c75640ec68da87a4df67133ce99c3a9d99b69
Sindhura-24 <31728600+Sindhura-24@users.noreply.github.com>
2019-01-10 01:23:43 +0530

    pagination for list company query

--- fennix-backend-app/repository-module/queries/company-query.js
+++ fennix-backend-app/repository-module/queries/company-query.js
@@ -1,5 +1,5 @@
 const addCompanyQuery = 'insert into ';
-const listCompanyQuery = 'select company_id, company_name, customs_id, (select localized_text from localization where language = $1 and locale_key = (select dropdown_value from dropdown_set where dropdown_set_id = c.company_type)) as company_type from company c where owner_user_id = $2';
+const listCompanyQuery = 'select company_id, company_name, customs_id, (select localized_text from localization where language = $1 and locale_key = (select dropdown_value from dropdown_set where dropdown_set_id = c.company_type)) as company_type from company c where owner_user_id = $2 order by updated_date desc nulls last offset $3 limit $4';
 
 module.exports = {
     addCompanyQuery,
