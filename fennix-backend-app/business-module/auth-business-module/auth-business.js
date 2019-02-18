cc674dccbec36e577d75f33ea39470259b543832
abhay lokesh <abjhontyharsh@gmail.com>
2019-02-19 02:37:36 +0530

    adding the forgot password flow

--- fennix-backend-app/business-module/auth-business-module/auth-business.js
+++ fennix-backend-app/business-module/auth-business-module/auth-business.js
@@ -1,12 +1,12 @@
-const crypto = require('crypto-js');
-const bcrypt = require('bcryptjs');
+const crypto = require('crypto-js');// this library is used to encrypt and decrypt password and email while transferring over the network
+const bcrypt = require('bcryptjs');// this library is used to generate the hash password
+const jwt = require('jsonwebtoken');// This method is used to implement the auth token to be ping ponged back and forth for the request authentication.
 const {statusCodeConstants} = require('../../util-module/status-code-constants');
 const {emoji} = require('../../util-module/custom-request-reponse-modifiers/encoder-decoder-constants');
 const {objectHasPropertyCheck, arrayNotEmptyCheck, responseObjectCreator} = require('../../util-module/data-validators');
 const {fennixResponse} = require('../../util-module/custom-request-reponse-modifiers/response-creator');
 const {checkUserEmailId, authenticateBeneficiaryDetails, authenticateUserDetails, checkBenificiaryEmailId} = require('../../repository-module/data-accesors/auth-accesor');
 const {fetchUserDetailsBusiness, userResetPasswordBusiness} = require('../user-business-module/user-business');
-const jwt = require('jsonwebtoken');
 const {forgotPasswordemailBusiness} = require('../common-business-module/common-business');
 
 const checkEmailId = async (req) => {
@@ -30,7 +30,14 @@ const fetchLoginProfileBusiness = async (req) => {
     loginProfileResponse = await fetchUserDetailsBusiness(req);
     return loginProfileResponse;
 };
-
+/**
+ * @description This method is used to authenticate the user.We will be receiving encrypted email and password from the login and from that we decrypt it.
+ * After decrypting we fetch the user based on email.We do a is active check via the retire check function.
+ * After this check is done we compare the decrypted password with the Hashed password from DB using *****bcrypt.compare*****
+ *@summary bcrypt is used to hash the password.So bcrypt will again be used to compare them as well.
+ * @param req
+ * @return {Promise<{response: null, header: null}>}
+ */
 const authenticateUser = async (req) => {
     let responseObj, businessResponse, authResponse, retireCheckFlag, returnResponse = {response: null, header: null};
     const algo = emoji[req.body.avatar]['encoding'];
@@ -77,21 +84,12 @@ const authenticateUser = async (req) => {
     return returnResponse;
 };
 
-const decrypt = (algo, passKey, message) => {
-    try {
-        const decryptedBytes = crypto[algo]['decrypt'](message, passKey);
-        return decryptedBytes.toString(crypto.enc.Utf8);
-    } catch (e) {
-        console.log(e);
-    }
-};
-
-const retireCheck = (responseObj) => (responseObj['isactive']);
-
-const authResponseObjectFormation = (responseObj) => {
-    return responseObjectCreator(responseObj, ['role_name', 'user_role', 'first_name', 'last_name', 'user_id', 'owner_user_id', 'email_id', 'isactive', 'center_id'], ['role_name', 'user_role', 'first_name', 'last_name', 'user_id', 'owner_user_id', 'email_id', 'isactive', 'center_id']);
-};
-
+/**
+ * @description When the user clicks on forgot password link in login page he is navigated to the forgot password page.
+ * There he enters the email and based on the email we validate and send out a forgot password link.
+ * @param req
+ * @return {Promise<{}|*|{}>}
+ */
 const forgotPasswordBusiness = async (req) => {
     let responseObj, businessResponse, retireCheckFlag, returnResponse = '';
     const algo = emoji[req.body.avatar]['encoding'];
@@ -128,23 +126,37 @@ const forgotPasswordBusiness = async (req) => {
     }
     return returnResponse;
 };
+
+/**
+ * @description This method is called when a user clicks on the forgot password link and the user enters the password and the reset password.
+ * We recieve the passwords and do a comparision if the passwords do match or not.If they dont we dont allow the user to change the password.
+ * To check if the passwords match or not we decrypt the password and check if both are same.
+ * @if Passwords match then we go ahead and hash the password +++++++15 rounds++++++++ using -------bcrypt.genSalt and bcrypt.hash-------
+ * After this process we send the success or failure error message to the UI.
+ * @param req
+ * @return {Promise<{}|*|*>}
+ */
 const resetPasswordBusiness = async (req) => {
-    let responseObj, businessResponse, retireCheckFlag, returnResponse = '';
+    let responseObj, hashedPassword, businessResponse, retireCheckFlag, returnResponse = '';
     const algo = emoji[req.body.avatar]['encoding'];
     const passKey = emoji[req.body.avatar]['secretPass'];
     const emailId = decrypt(algo, passKey, req.body.email);
-    const password = req.body.password.toString();
-    console.log(password);
+    const password = decrypt(algo, passKey, req.body.password);
     const request = [
         emailId,
         req.body.language
     ];
+    await bcrypt.genSalt(15, async (err, salt) => {
+        await bcrypt.hash(password, salt, (err, hash) => {
+            hashedPassword = hash;
+        });
+    });
     businessResponse = await authenticateUserDetails(request);
     if (objectHasPropertyCheck(businessResponse, 'rows') && arrayNotEmptyCheck(businessResponse.rows)) {
         retireCheckFlag = retireCheck(businessResponse.rows[0]);
         responseObj = responseFormation(businessResponse.rows[0], retireCheckFlag);
         if (retireCheckFlag) {
-            returnResponse = await userResetPasswordBusiness(emailId, password);
+            returnResponse = await userResetPasswordBusiness(emailId, hashedPassword);
         } else {
             returnResponse = fennixResponse(statusCodeConstants.STATUS_NO_USER_FOR_ID, 'EN_US', []);
         }
@@ -162,10 +174,48 @@ const resetPasswordBusiness = async (req) => {
     }
     return returnResponse;
 };
+/**
+ *
+ * @param algo - defines the algorithm being used to decrypt.Eg:SHA256,MD5
+ * @param passKey - This is the password that we want to decrypt
+ * @param message - This is the secret message being sent from the UI via avatar.
+ * @return {string}
+ */
+const decrypt = (algo, passKey, message) => {
+    try {
+        const decryptedBytes = crypto[algo]['decrypt'](message, passKey);
+        return decryptedBytes.toString(crypto.enc.Utf8);
+    } catch (e) {
+        console.log(e);
+    }
+};
 
+/**
+ *@description This method performs basic isActive check for the given user.
+ * @param responseObj
+ * @return {*}
+ */
+const retireCheck = (responseObj) => (responseObj['isactive']);
+
+/**
+ * @description this creates the response in the format that the UI expects.
+ * @param responseObj
+ * @param retireCheck
+ * @return {{}|*}
+ */
 const responseFormation = (responseObj, retireCheck) => {
     return retireCheck ? fennixResponse(statusCodeConstants.STATUS_USER_AUTHENTICATED, 'EN_US', responseObj) : fennixResponse(statusCodeConstants.STATUS_USER_RETIRED, 'EN_US', responseObj)
 };
+
+/**
+ * @description Tis method creates the authentication object.
+ * @param responseObj
+ * @return {*}
+ */
+const authResponseObjectFormation = (responseObj) => {
+    return responseObjectCreator(responseObj, ['role_name', 'user_role', 'first_name', 'last_name', 'user_id', 'owner_user_id', 'email_id', 'isactive', 'center_id'], ['role_name', 'user_role', 'first_name', 'last_name', 'user_id', 'owner_user_id', 'email_id', 'isactive', 'center_id']);
+};
+
 module.exports = {
     checkEmailId,
     authenticateUser,
