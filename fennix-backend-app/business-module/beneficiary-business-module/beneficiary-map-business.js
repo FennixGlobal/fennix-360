82cae3d9cf203c9194068bc125cc28048375170a
abhay lokesh <abjhontyharsh@gmail.com>
2019-02-21 09:03:35 +0530

    adding the refactor for map

--- fennix-backend-app/business-module/beneficiary-business-module/beneficiary-map-business.js
+++ fennix-backend-app/business-module/beneficiary-business-module/beneficiary-map-business.js
@@ -1,5 +1,5 @@
 const beneficiaryAccessor = require('../../repository-module/data-accesors/beneficiary-accesor');
-const {objectHasPropertyCheck, deviceStatusMapper, arrayNotEmptyCheck, notNullCheck} = require('../../util-module/data-validators');
+const {objectHasPropertyCheck, deviceStatusMapper, arrayNotEmptyCheck, notNullCheck, responseObjectCreator} = require('../../util-module/data-validators');
 const {fennixResponse} = require('../../util-module/custom-request-reponse-modifiers/response-creator');
 const {statusCodeConstants} = require('../../util-module/status-code-constants');
 const {deviceByBeneficiaryIdAccessor} = require('../../repository-module/data-accesors/device-accesor');
@@ -26,110 +26,114 @@ const beneficiaryTrackMapBusiness = async (req) => {
         let beneficiaryIdListAndDetailObj, beneficiaryDeviceArray;
         beneficiaryIdListAndDetailObj = beneficiaryListResponse.rows.reduce((init, item) => {
             init.beneficiaryIdArray.push(parseInt(item.beneficiaryid));
-            init.beneficiaryDetailObj[item.beneficiaryid] = {
-                beneficiaryId: item['beneficiaryid'],
-                firstName: item['firstname'],
-                documentId: item['document_id'],
-                mobileNo: item['mobileno'],
-                image: item['image'],
-                emailId: item['emailid'],
-                // roleName: item['role'],
-                beneficiaryRoleId: item['role_id'],
-                gender: item['gender']
-            };
+            //     {
+            //
+            //     beneficiaryId: item['beneficiaryid'],
+            //     firstName: item['firstname'],
+            //     documentId: item['document_id'],
+            //     mobileNo: item['mobileno'],
+            //     image: item['image'],
+            //     emailId: item['emailid'],
+            //     beneficiaryRoleId: item['role_id'],
+            //     gender: item['gender']
+            // };
+            init.beneficiaryDetailObj[item.beneficiaryid] =
+                responseObjectCreator(item, ['beneficiaryId', 'firstName', 'documentId', 'mobileNo', 'image', 'emailId', 'beneficiaryRoleId', 'gender'], ['beneficiaryid', 'firstname', 'document_id', 'mobileno', 'image', 'emailid', 'role_id', 'gender'])
             return init;
         }, {beneficiaryIdArray: [], beneficiaryDetailObj: {}});
         beneficiaryDeviceArray = await deviceByBeneficiaryIdAccessor(beneficiaryIdListAndDetailObj.beneficiaryIdArray);
-        beneficiaryDeviceArray.forEach((item) => {
-            locationObj[item.beneficiaryId] = {...beneficiaryIdListAndDetailObj['beneficiaryDetailObj'][item.beneficiaryId]};
-            locationObj[item.beneficiaryId]['location'] = {
-                longitude: item.location.longitude,
-                latitude: item.location.latitude
-            };
-            beneficiaryIdListAndDetailObj['beneficiaryDetailObj'][item.beneficiaryId]['imei'] = item['device']['imei'];
-            locationObj[item.beneficiaryId]['roleId'] = beneficiaryIdListAndDetailObj['beneficiaryDetailObj'][item.beneficiaryId]['roleId'];
-            const deviceDetails = {};
-            let noOfViolations = 0;
-            deviceDetails[item.beneficiaryId] = [];
-            const GPS = {A: 'Valid', V: 'Invalid'};
-            const batteryPercentage = deviceStatusMapper('batteryPercentage', item.deviceAttributes.batteryPercentage);
-            if (batteryPercentage['deviceStatus'] === 'violation') {
-                noOfViolations += 1;
-            }
-            if (item.deviceAttributes.beltStatus) {
-                noOfViolations += 1;
-            }
-            if (item.deviceAttributes.shellStatus) {
-                noOfViolations += 1;
-            }
-            deviceDetails[item.beneficiaryId].push({
-                text: 'Battery',
-                status: batteryPercentage['deviceStatus'],
-                key: 'batteryPercentage',
-                icon: 'battery_charging_full',
-                value: `${item.deviceAttributes.batteryPercentage}%`
+        if (arrayNotEmptyCheck(beneficiaryDeviceArray)) {
+            beneficiaryDeviceArray.forEach((item) => {
+                locationObj[item.beneficiaryId] = {...beneficiaryIdListAndDetailObj['beneficiaryDetailObj'][item.beneficiaryId]};
+                locationObj[item.beneficiaryId]['location'] = {
+                    longitude: item.location.longitude,
+                    latitude: item.location.latitude
+                };
+                beneficiaryIdListAndDetailObj['beneficiaryDetailObj'][item.beneficiaryId]['imei'] = item['device']['imei'];
+                locationObj[item.beneficiaryId]['roleId'] = beneficiaryIdListAndDetailObj['beneficiaryDetailObj'][item.beneficiaryId]['roleId'];
+                const deviceDetails = {};
+                let noOfViolations = 0;
+                deviceDetails[item.beneficiaryId] = [];
+                const GPS = {A: 'Valid', V: 'Invalid'};
+                const batteryPercentage = deviceStatusMapper('batteryPercentage', item.deviceAttributes.batteryPercentage);
+                if (batteryPercentage['deviceStatus'] === 'violation') {
+                    noOfViolations += 1;
+                }
+                if (item.deviceAttributes.beltStatus) {
+                    noOfViolations += 1;
+                }
+                if (item.deviceAttributes.shellStatus) {
+                    noOfViolations += 1;
+                }
+                deviceDetails[item.beneficiaryId].push({
+                    text: 'Battery',
+                    status: batteryPercentage['deviceStatus'],
+                    key: 'batteryPercentage',
+                    icon: 'battery_charging_full',
+                    value: `${item.deviceAttributes.batteryPercentage}%`
+                });
+                deviceDetails[item.beneficiaryId].push({
+                    text: 'Belt',
+                    key: 'beltStatus',
+                    icon: 'link',
+                    status: item.deviceAttributes.beltStatus === 1 ? 'violation' : 'safe',
+                    value: item.deviceAttributes.beltStatus === 1 ? 'belt' : 'OK'
+                });
+                deviceDetails[item.beneficiaryId].push({
+                    text: 'Shell',
+                    key: 'shellStatus',
+                    icon: 'lock',
+                    status: item.deviceAttributes.shellStatus === 1 ? 'violation' : 'safe',
+                    value: item.deviceAttributes.shellStatus === 1 ? 'shell' : 'OK'
+                });
+                deviceDetails[item.beneficiaryId].push({
+                    text: 'GSM',
+                    key: 'gmsStatus',
+                    icon: 'signal_cellular_4_bar',
+                    status: item.deviceAttributes.gsmSignal < 2 ? 'violation' : 'safe',
+                    value: item.deviceAttributes.gsmSignal < 2 ? 'Low' : 'OK'
+                });
+                deviceDetails[item.beneficiaryId].push({
+                    text: 'RF Home',
+                    key: 'rfConnectionStatus',
+                    icon: 'home',
+                    status: item.deviceAttributes.rfConnectionStatus === 0 ? 'violation' : 'safe',
+                    value: item.deviceAttributes.rfConnectionStatus === 0 ? 'Outdoor' : 'Home'
+                });
+                deviceDetails[item.beneficiaryId].push({
+                    text: 'RFID',
+                    key: 'rfPlugStatus',
+                    icon: 'rss_feed',
+                    status: item.deviceAttributes.rfPlugStatus === 0 ? 'violation' : 'safe',
+                    value: item.deviceAttributes.rfPlugStatus === 0 ? 'Out' : 'In'
+                });
+                deviceDetails[item.beneficiaryId].push({
+                    text: 'SAT',
+                    key: 'gpsStatus',
+                    icon: 'gps_fixed',
+                    status: item.deviceAttributes.gpsStatus === 'V' ? 'violation' : 'safe',
+                    value: GPS[item.deviceAttributes.gpsStatus]
+                });
+                deviceDetails[item.beneficiaryId].push({
+                    text: 'Speed',
+                    key: 'speed',
+                    icon: 'directions_run',
+                    status: item.deviceAttributes.speed > 0 ? 'moving' : 'still',
+                    value: Math.floor(item.deviceAttributes.speed)
+                });
+                beneficiaryDevices = {...deviceDetails};
+                const completeDate = new Date(`${item.deviceAttributes.deviceUpdatedDate}`);
+                const modifiedDate = `${completeDate.toLocaleDateString('es')} ${completeDate.toLocaleTimeString()}`;
+                beneficiaryIdListAndDetailObj.beneficiaryDetailObj[item.beneficiaryId]['deviceUpdatedDate'] = modifiedDate;
+                beneficiaryIdListAndDetailObj.beneficiaryDetailObj[item.beneficiaryId]['deviceDetails'] = deviceDetails[item.beneficiaryId];
+                beneficiaryIdListAndDetailObj.beneficiaryDetailObj[item.beneficiaryId]['noOfViolations'] = {
+                    text: 'Number of Violations',
+                    value: noOfViolations
+                };
+                locationObj[item.beneficiaryId]['noOfViolations'] = noOfViolations;
+                gridData[item.beneficiaryId] = {...beneficiaryIdListAndDetailObj.beneficiaryDetailObj[item.beneficiaryId]};
             });
-            deviceDetails[item.beneficiaryId].push({
-                text: 'Belt',
-                key: 'beltStatus',
-                icon: 'link',
-                status: item.deviceAttributes.beltStatus === 1 ? 'violation' : 'safe',
-                value: item.deviceAttributes.beltStatus === 1 ? 'belt' : 'OK'
-            });
-            deviceDetails[item.beneficiaryId].push({
-                text: 'Shell',
-                key: 'shellStatus',
-                icon: 'lock',
-                status: item.deviceAttributes.shellStatus === 1 ? 'violation' : 'safe',
-                value: item.deviceAttributes.shellStatus === 1 ? 'shell' : 'OK'
-            });
-            deviceDetails[item.beneficiaryId].push({
-                text: 'GSM',
-                key: 'gmsStatus',
-                icon: 'signal_cellular_4_bar',
-                status: item.deviceAttributes.gsmSignal < 2 ? 'violation' : 'safe',
-                value: item.deviceAttributes.gsmSignal < 2 ? 'Low' : 'OK'
-            });
-            deviceDetails[item.beneficiaryId].push({
-                text: 'RF Home',
-                key: 'rfConnectionStatus',
-                icon: 'home',
-                status: item.deviceAttributes.rfConnectionStatus === 0 ? 'violation' : 'safe',
-                value: item.deviceAttributes.rfConnectionStatus === 0 ? 'Outdoor' : 'Home'
-            });
-            deviceDetails[item.beneficiaryId].push({
-                text: 'RFID',
-                key: 'rfPlugStatus',
-                icon: 'rss_feed',
-                status: item.deviceAttributes.rfPlugStatus === 0 ? 'violation' : 'safe',
-                value: item.deviceAttributes.rfPlugStatus === 0 ? 'Out' : 'In'
-            });
-            deviceDetails[item.beneficiaryId].push({
-                text: 'SAT',
-                key: 'gpsStatus',
-                icon: 'gps_fixed',
-                status: item.deviceAttributes.gpsStatus === 'V' ? 'violation' : 'safe',
-                value: GPS[item.deviceAttributes.gpsStatus]
-            });
-            deviceDetails[item.beneficiaryId].push({
-                text: 'Speed',
-                key: 'speed',
-                icon: 'directions_run',
-                status: item.deviceAttributes.speed > 0 ? 'moving' : 'still',
-                value: Math.floor(item.deviceAttributes.speed)
-            });
-            beneficiaryDevices = {...deviceDetails};
-            const completeDate = new Date(`${item.deviceAttributes.deviceUpdatedDate}`);
-            const modifiedDate = `${completeDate.toLocaleDateString('es')} ${completeDate.toLocaleTimeString()}`;
-            beneficiaryIdListAndDetailObj.beneficiaryDetailObj[item.beneficiaryId]['deviceUpdatedDate'] = modifiedDate;
-            beneficiaryIdListAndDetailObj.beneficiaryDetailObj[item.beneficiaryId]['deviceDetails'] = deviceDetails[item.beneficiaryId];
-            beneficiaryIdListAndDetailObj.beneficiaryDetailObj[item.beneficiaryId]['noOfViolations'] = {
-                text: 'Number of Violations',
-                value: noOfViolations
-            };
-            locationObj[item.beneficiaryId]['noOfViolations'] = noOfViolations;
-            gridData[item.beneficiaryId] = {...beneficiaryIdListAndDetailObj.beneficiaryDetailObj[item.beneficiaryId]};
-        });
+        }
         beneficiaryReturnObj['markers'] = Object.keys(locationObj).map(key => locationObj[key]);
         beneficiaryReturnObj['deviceDetails'] = beneficiaryDevices;
         beneficiaryReturnObj['deviceDetailsArray'] = Object.keys(beneficiaryDevices).map((device) => beneficiaryDevices[device]);
@@ -209,6 +213,6 @@ const getBeneficiaryMapHistoryBusiness = async (req) => {
 };
 
 module.exports = {
-  beneficiaryTrackMapBusiness,
-  getBeneficiaryMapHistoryBusiness
+    beneficiaryTrackMapBusiness,
+    getBeneficiaryMapHistoryBusiness
 };
