2374670bb9ca5c50f2ba7307bf09cfc1d7d51950
abhay lokesh <abjhontyharsh@gmail.com>
2019-03-19 00:30:25 +0530

    Adding the user session collection name change

--- fennix-backend-app/repository-module/models/user-session-model.js
+++ fennix-backend-app/repository-module/models/user-session-model.js
@@ -19,7 +19,7 @@ const UserSessionSchema = new Schema({
     }]
 });
 
-const UserSessionModel = mongoose.model('userSession',UserSessionSchema, 'UserSessions');
+const UserSessionModel = mongoose.model('UserSessions', UserSessionSchema);
 
 UserSessionSchema.statics.findUserByEmail = async function (emailId) {
     let userSession = UserSessionModel.findOne({userEmailId: emailId});
