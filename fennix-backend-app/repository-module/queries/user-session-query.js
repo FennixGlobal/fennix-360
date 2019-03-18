1630964c2f58707a1cec8cde33cfe1d460c33498
abhay lokesh <abjhontyharsh@gmail.com>
2019-03-18 18:39:58 +0530

    Adding the user session collection name change

--- fennix-backend-app/repository-module/queries/user-session-query.js
+++ fennix-backend-app/repository-module/queries/user-session-query.js
@@ -1,6 +1,7 @@
 const {UserSessionModel} = require('../models/user-session-model');
 
 const generateAuthTokenQuery = async(userObj,authType)=>{
+    console.log(UserSessionModel);
     return await UserSessionModel.generateAuthToken(userObj,authType);
 };
 
