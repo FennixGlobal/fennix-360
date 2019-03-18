aeab4e5d06b0cb145dac096ceb7be5d42ef9e1f6
abhay lokesh <abjhontyharsh@gmail.com>
2019-03-19 01:48:34 +0530

    Adding the user session model as a new constructed object

--- fennix-backend-app/repository-module/queries/user-session-query.js
+++ fennix-backend-app/repository-module/queries/user-session-query.js
@@ -1,7 +1,7 @@
 const UserSessionModel = require('../models/user-session-model');
 
 const generateAuthTokenQuery = async (userObj, authType) => {
-    console.log(UserSessionModel);
+    console.log(userObj);
     const userSessionModel = await UserSessionModel.findUserByEmail(userObj.email_id);
     console.log(userSessionModel);
     return await userSessionModel.generateAuthToken(userObj, authType);
