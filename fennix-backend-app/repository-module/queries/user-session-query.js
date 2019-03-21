45854f08e774acb8c54539e60caeedda92a990cc
abhay lokesh <abjhontyharsh@gmail.com>
2019-03-21 13:37:57 +0530

    retriving ip address

--- fennix-backend-app/repository-module/queries/user-session-query.js
+++ fennix-backend-app/repository-module/queries/user-session-query.js
@@ -6,8 +6,11 @@ const generateAuthTokenQuery = async (userObj, authType, ip) => {
 };
 const generateCookieTokenQuery = async (userObj, authType, ip) => {
     const userSessionModel = await UserSessionModel.findUserByEmail(userObj.email_id);
+    console.log(userSessionModel);
     const cookie = await userSessionModel.generateCookieToken(userObj, authType, ip);
+
     const userSessionModel2 = await UserSessionModel.findUserByEmail(userObj.email_id);
+    console.log(userSessionModel2);
     const token = await userSessionModel2.generateAuthToken(userObj, authType, ip);
     return {cookie, token};
 };
