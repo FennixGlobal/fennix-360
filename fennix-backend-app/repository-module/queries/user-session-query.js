9f57946cba49f58916fc0987b6b5a1a79a86fe4f
abhay lokesh <abjhontyharsh@gmail.com>
2019-03-21 14:14:46 +0530

    Adding the cookie logic with update

--- fennix-backend-app/repository-module/queries/user-session-query.js
+++ fennix-backend-app/repository-module/queries/user-session-query.js
@@ -6,8 +6,8 @@ const generateAuthTokenQuery = async (userObj, authType, ip) => {
 };
 const generateCookieTokenQuery = async (userObj, authType, ip) => {
     const userSessionModel = await UserSessionModel.findUserByEmail(userObj.email_id);
-    const cookie = await userSessionModel.generateCookieToken(userObj, authType, ip);
     const token = await userSessionModel.generateAuthToken(userObj, authType, ip);
+    const cookie = await userSessionModel.generateCookieToken(userObj, authType, ip);
     return {cookie, token};
 };
 
